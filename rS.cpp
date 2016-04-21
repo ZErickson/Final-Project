8374e0b562a5bb487061e1a17aaab806a1fb1c8a
Sydni Baluch <sbaluch@carthage.edu>
2016-04-21 10:55:49 -0500

    Added files via upload

---
+++ rS.cpp
@@ -0,0 +1,1 @@
+#include "rS.h"
---
+++ rS.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "character.h"
+#include "word.h"
+
+using namespace std;
+
+class rS {
+	vector <Character> racialIdentifiers;
+	vector <string> middleEastMuslimStereotypes;
+	vector <string> nativeAmericanStereotypes;
+	vector <string> latinoStereotypes;
+	vector <string> africanAmericanStereotypes;
+	vector <string> genPOCStereotypes;
+};
